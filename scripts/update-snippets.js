6056b61535e657065e553159d12e317fb3f48143
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2016-12-28 11:27:14 +0000

    Pull in snippets from atom package

--- scripts/update-snippets.js
+++ scripts/update-snippets.js
@@ -3,14 +3,14 @@ var cson = require('cson');
 var process = require('process');
 var fs = require('fs');
 
-var url = 'https://raw.githubusercontent.com/purescript-contrib/atom-language-purescript/master/grammars/purescript.cson';
+var url = 'https://raw.githubusercontent.com/purescript-contrib/atom-language-purescript/master/snippets/language-purescript.cson';
 request(url, function (error, response, body) {
 	if (error) {
 		console.error(error);
 		process.exit(1);
 	} else {
-		var obj = cson.parse(body);
+		var obj = cson.parse(body)[".source.purescript"];
 		var json = cson.createJSONString(obj);
-		fs.writeFileSync('syntaxes/purescript.json', json)
+		fs.writeFileSync('snippets/purescript.json', json)
 	}
 });
