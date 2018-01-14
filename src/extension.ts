6373b835af1aaa4630c3fb8aedf2659aac669e43
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2018-01-14 21:54:31 +0000

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 
 // Configure in code as wordPattern not supported in JSON format
 const configuration : vscode.LanguageConfiguration = {
-    wordPattern: /\\[^\s]+|[^\\\s\d(){}\[\]#.][^\\\s(){}\[\]#.]*/,
+    wordPattern: /([\w'_][\w'_\d]*)|([0-9]+\.[0-9]+([eE][+-]?[0-9]+)?|[0-9]+[eE][+-]?[0-9]+)/,
     "comments": {
 		// symbol used for single line comment. Remove this entry if your language does not support line comments
 		"lineComment": "--",
