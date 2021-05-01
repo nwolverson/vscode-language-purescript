9f5585bad21628ef8a2d642a84651869ff7d9c59
wclr <alex.oshchepkov@gmail.com>
2021-05-01 16:43:05 +0500

    add local/remote update script

--- scripts/update-snippets.js
+++ scripts/update-snippets.js
@@ -1,16 +1,3 @@
-var request = require('request');
-var cson = require('cson');
-var process = require('process');
-var fs = require('fs');
+process.argv.push("--snippets");
 
-var url = 'https://raw.githubusercontent.com/purescript-contrib/atom-language-purescript/master/snippets/language-purescript.cson';
-request(url, function (error, response, body) {
-	if (error) {
-		console.error(error);
-		process.exit(1);
-	} else {
-		var obj = cson.parse(body)[".source.purescript"];
-		var json = cson.createJSONString(obj);
-		fs.writeFileSync('snippets/purescript.json', json)
-	}
-});
+require("./update");
